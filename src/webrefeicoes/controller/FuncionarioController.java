69c5ff31eaa200d31ab356638b38e9958ec6a76e
thiag <thiag@192.168.2.5>
2017-01-21 12:48:25 -0200

    colocado o insert no banco

--- src/webrefeicoes/controller/FuncionarioController.java
+++ src/webrefeicoes/controller/FuncionarioController.java
@@ -1,5 +1,27 @@
 package webrefeicoes.controller;
 
-public class FuncionarioController {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import webrefeicoes.model.Funcionario;
+
+public class FuncionarioController {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("WebRefeicoes");
+		EntityManager em = factory.createEntityManager();
+		
+		Funcionario func = new Funcionario();
+		
+		func.setNome("Thiago Armele de Campos");
+		
+		em.getTransaction().begin();
+		
+		em.persist(func);
+
+		em.getTransaction().commit();
+	}
+	
+	
 }
